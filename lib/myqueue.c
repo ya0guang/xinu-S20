d4aa53a149543381ccf9a40e8a98008e7984e408
ya0guang <ya0guang@github.com>
2020-02-20 22:13:02 -0500

    future debugging

--- lib/myqueue.c
+++ lib/myqueue.c
@@ -5,7 +5,7 @@ int size_myqueue(myqueue_t *q)
     int result;
     if (q->head <= q->tail)
     {
-        printf("head: %d, tail: %d minus: %d \n", q->head, q->tail, q->tail - q->head);
+        // printf("head: %d, tail: %d minus: %d \n", q->head, q->tail, q->tail - q->head);
         result = q->tail - q->head;
     }
     else
